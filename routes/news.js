9f70abfb22777399915a7c2338b8ec4c14cb5abd
adhyanagpal <adhyanagpal@gmail.com>
2021-06-27 02:19:05 +0530

    Add basic features

--- routes/news.js
+++ routes/news.js
@@ -6,42 +6,27 @@ const NewsAPI = require('newsapi');
 const newsapi = new NewsAPI(key);
 
 route.get('/get_news', (req,res)=>{
-    // fetch("https://newsapi.org/v2/everything&apiKey="+key)
-    // .then(response => response.json())
-    // .then(data => console.log(data))
-    newsapi.v2.topHeadlines({
-        //q: 'trump',
-        //category: 'politics',
-        language: 'en',
-        //country: 'india'
-      }).then(response => {
-        //console.log(response['articles'][0]);
-        //res.send(response['articles'])
-        //html = response['articles'].map(obj=>{
-            obj = response['articles'][0]
-            source_name = obj.source.name
-            author = obj.author
-            title = obj.title
-            desc = obj.description
-            url = obj.url
-            img_url = obj.urlToImage
-            timestamp = obj.publishedAt
-            content = obj.content
-            html_obj = `<h2>${title}</h2>
-                        <h3>${desc}</h3>
-                        <p>${content}</p>
-                        <br>
-                        ${author}, ${source_name}
-                        <br>
-                        <a href =${url}> Click here to view full article</a>
-                        `
-            res.send(html_obj)
-        //})
-      });
+  newsapi.v2.topHeadlines({
+    language: 'en',
+    country: 'in'
+  }).then(response => {
+    let arr = response['articles']
+    res.render('general',{arr})
+    //console.log(response['articles']);
+  });
 })
 
 route.post('/get_news', (req,res)=>{
-    console.log(req.body.params.length())
+  newsapi.v2.topHeadlines({
+    language: 'en',
+    country: 'in',
+    q: req.body.query
+  }).then(response => {
+    let arr = response['articles']
+    res.render('general',{arr})
+    //console.log(response['articles']);
+  });
+  //console.log(req.body.query)
 })
 
 module.exports = route
--- server.js
+++ server.js
@@ -1,16 +1,24 @@
-const express = require('express')
+const express=require('express')
 const app=express()
+const hbs=require('hbs')
 const path=require('path')
 
-app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+app.set('view engine','hbs')
+
 app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+hbs.registerPartials(path.join(__dirname,'/partials'))
+
+app.use(express.static(__dirname+'/public'))
 
 const newsRoute=require('./routes/news.js')
 
 app.use(newsRoute)
 
 app.get('/',(req,res)=>{
-    res.sendFile(path.join(__dirname+ "/public/index.html"))
+    // res.sendFile(path.join(__dirname+ "/public/index.html"))
+    res.render('index')
 })
 
-app.listen(3000)
+app.listen(2000)
